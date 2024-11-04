9533fb1ac5f9a5b9ddd3dd35f1b7bc151ad46716
Pranav2708 <123442111+Pranav2708@users.noreply.github.com>
2024-11-04 18:43:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,24 @@
-import { ReactNode } from "react";
-import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
+import { ReactNode } from 'react';
+import type { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Poppins } from 'next/font/google';
 
-import "@stream-io/video-react-sdk/dist/css/styles.css";
-import "react-datepicker/dist/react-datepicker.css";
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '700'], // Specify the font weights you want to use
+  style: ['normal', 'italic'],
+});
 
 export const metadata: Metadata = {
-  title: "YOOM",
-  description: "Video calling App",
+  title: 'EmotiCam',
+  description: 'Video calling App',
   icons: {
-    icon: "/icons/logo.svg",
+    icon: '/icons/logo.jpg',
   },
 };
 
@@ -26,19 +30,19 @@ export default function RootLayout({
       <ClerkProvider
         appearance={{
           layout: {
-            socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+            socialButtonsVariant: 'iconButton',
+            logoImageUrl: '/icons/logo.svg',
           },
           variables: {
-            colorText: "#fff",
-            colorPrimary: "#0E78F9",
-            colorBackground: "#1C1F2E",
-            colorInputBackground: "#252A41",
-            colorInputText: "#fff",
+            colorText: '#fff',
+            colorPrimary: '#0E78F9',
+            colorBackground: '#1C1F2E',
+            colorInputBackground: '#252A41',
+            colorInputText: '#fff',
           },
         }}
       >
-        <body className={`${inter.className} bg-dark-2`}>
+        <body className={`${poppins.className} bg-dark-2`}>
           <Toaster />
           {children}
         </body>
